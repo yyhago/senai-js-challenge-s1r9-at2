624e9a9c27a427a9c0aed5b4cc82575a687cf322
yyhago <yhago.felipe.teles@gmail.com>
2025-05-21 15:08:17 -0300

    first commit

---
+++ challenge02/script.js
@@ -0,0 +1,2 @@
+// 2 - Desenvolva um programa que leia 10 números inteiros e guarde-os em um vetor. 
+// No final, mostre quais são os números pares que foram digitados e em que posições eles estão armazenados.
